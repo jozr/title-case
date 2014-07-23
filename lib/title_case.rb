e9b027a3f144b7ad7d0fcc4207a012d46668814c
Josie R Wright & Amy Vaillancourt-Sals <student@epicodus.com>
2014-07-23 14:41:53 -0700

    add exemptions for little words

--- lib/title_case.rb
+++ lib/title_case.rb
@@ -1,17 +1,22 @@
 def title_case(input)
   input = input.downcase()
-  split_phrase = input.split(' ')
+  split_phrase = input.split(" ")
   final_phrase = []
+
 
   split_phrase.each do |i|
 
-    first_letter = i[0].upcase
-    rest_of_word = i[1..-1]
-    full_word = first_letter + rest_of_word
-    final_phrase << full_word
-
+    unless (i == "a") || (i == "of") || (i == "but") || (i == "if") || (i == "as") || (i == "is") || (i == "at") ||
+      (i == "and") || (i == "from") || (i == "to") || (i == "for") || (i == "yet") || (i == "so") || (i == "the")
+      first_letter = i[0].upcase
+      rest_of_word = i[1..-1]
+      full_word = first_letter + rest_of_word
+      final_phrase << full_word
+    else
+      final_phrase << i
+     end
   end
   final_phrase.join(' ')
 end
 
-title_case("FOREVER YOUNG")
+title_case("forever young and happy")

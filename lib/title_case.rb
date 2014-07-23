a36929a54018305dd7c2189ff267f5cb7a92085a
Josie R Wright & Amy Vaillancourt-Sals <student@epicodus.com>
2014-07-23 14:54:28 -0700

    all working

--- lib/title_case.rb
+++ lib/title_case.rb
@@ -5,16 +5,24 @@ def title_case(input)
 
 
   split_phrase.each do |i|
+    unless i == split_phrase[0]
 
-    unless (i == "a") || (i == "of") || (i == "but") || (i == "if") || (i == "as") || (i == "is") || (i == "at") ||
-      (i == "and") || (i == "from") || (i == "to") || (i == "for") || (i == "yet") || (i == "so") || (i == "the")
+      unless (i == "a") || (i == "of") || (i == "but") || (i == "if") || (i == "as") || (i == "is") || (i == "at") ||
+        (i == "and") || (i == "from") || (i == "to") || (i == "for") || (i == "yet") || (i == "so") || (i == "the") || (i == "in")
+        first_letter = i[0].upcase
+        rest_of_word = i[1..-1]
+        full_word = first_letter + rest_of_word
+        final_phrase << full_word
+      else
+        final_phrase << i
+      end
+
+    else
       first_letter = i[0].upcase
       rest_of_word = i[1..-1]
       full_word = first_letter + rest_of_word
       final_phrase << full_word
-    else
-      final_phrase << i
-     end
+    end
   end
   final_phrase.join(' ')
 end
--- spec/title_case_spec.rb
+++ spec/title_case_spec.rb
@@ -14,4 +14,7 @@ describe('title_case') do
     expect(title_case('forever young and happy')).to(eq('Forever Young and Happy'))
   end
 
+  it('knows to keep the first word capitalized even if the first word is small') do
+    expect(title_case('a day in the life')).to(eq('A Day in the Life'))
+  end
 end
